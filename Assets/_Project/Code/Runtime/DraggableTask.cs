03896153dc5bca66ce7e6415c6f27851ffee8a52
bitemdev <bitemgamedev@gmail.com>
2026-03-27 20:12:13 +0100

    feat: add ui to the app

---
+++ Assets/_Project/Code/Runtime/DraggableTask.cs
@@ -0,0 +1,49 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+[RequireComponent(typeof(CanvasGroup))]
+public class DraggableTask : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+{
+    [HideInInspector] public Transform OriginalParent;
+    private CanvasGroup _canvasGroup;
+
+    private void Awake()
+    {
+        _canvasGroup = GetComponent<CanvasGroup>();
+    }
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        // Remember where it came from in case it is dropped in an invalid spot
+        OriginalParent = transform.parent;
+
+        // Move the card to the very top level of the Canvas so it doesn't get clipped by the ScrollView
+        transform.SetParent(transform.root);
+        transform.SetAsLastSibling(); // Renders on top of everything else
+
+        // Turn off raycasts so the DropZone behind the card can detect the mouse
+        _canvasGroup.blocksRaycasts = false;
+        
+        _canvasGroup.alpha = 0.8f; 
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        // Snap the card to the mouse position
+        transform.position = Input.mousePosition;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        // Re-enable raycasts and reset alpha
+        _canvasGroup.blocksRaycasts = true;
+        _canvasGroup.alpha = 1f;
+
+        // If the parent is still the root canvas, it means it wasn't dropped on a valid DropZone.
+        // Snap it back to its original column.
+        if (transform.parent == transform.root)
+        {
+            transform.SetParent(OriginalParent);
+        }
+    }
+}
---
+++ Assets/_Project/Code/Runtime/DropZone.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DropZone : MonoBehaviour, IDropHandler
+{
+    [SerializeField] private TaskStatus _columnStatus; 
+    [SerializeField] private Transform _contentContainer; 
+
+    public void OnDrop(PointerEventData eventData)
+    {
+        // Check if the thing being dropped has the DraggableTask script
+        if (eventData.pointerDrag != null)
+        {
+            DraggableTask draggedTask = eventData.pointerDrag.GetComponent<DraggableTask>();
+            if (draggedTask != null)
+            {
+                // Re-parent the card to this column's content container
+                draggedTask.transform.SetParent(_contentContainer);
+                
+                // Update the original parent so it doesn't snap back to the old column
+                draggedTask.OriginalParent = _contentContainer;
+
+                Debug.Log($"Task moved to {_columnStatus}!");
+            }
+        }
+    }
+}
